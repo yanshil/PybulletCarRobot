a69089d8051768fb87849eb81df906ef497d39db
Yanshi Luo <kdrobian@hotmail.com>
2020-06-12 14:40:46 -0400

    Add 3 models

---
+++ Pybullet_racecar/info_dict.py
@@ -0,0 +1,14 @@
+# {'right_rear_wheel_joint': 3, 'right_front_wheel_joint': 7, 'left_rear_wheel_joint': 2, 'left_front_wheel_joint': 5}
+info_dict = {
+    'jointId': {3: 'BackRight',
+                7: 'FrontRight',
+                2: 'BackLeft',
+                5: 'FrontLeft'},
+    'init_motorStatus': {3: {'controlMode': 0, 'targetVelocity': -13, 'force': 5},
+                            7: {'controlMode': 0, 'targetVelocity': 13, 'force': 5},
+                            2: {'controlMode': 0, 'targetVelocity': 2.7, 'force': 5},
+                            5: {'controlMode': 0, 'targetVelocity': 14.7, 'force': 5}},
+    'init_physical_para': {3: {'lateralFriction': .7, 'angularDamping': 0, 'linearDamping': 0, 'rollingFriction': 0, 'spinningFriction': 0},
+                            7: {'lateralFriction': .7, 'angularDamping': 0, 'linearDamping': 0, 'rollingFriction': 0, 'spinningFriction': 0},
+                            2: {'lateralFriction': .7, 'angularDamping': 0, 'linearDamping': 0, 'rollingFriction': 0, 'spinningFriction': 0},
+                            5: {'lateralFriction': .7, 'angularDamping': 0, 'linearDamping': 0, 'rollingFriction': 0, 'spinningFriction': 0}}}
---
+++ simple_cuboid_4074g/info_dict.py
@@ -0,0 +1,14 @@
+## {'BR': 1, 'FR': 2, 'BL': 0, 'FL': 3}
+info_dict = {
+    'jointId': {1: 'BackRight',
+                2: 'FrontRight',
+                0: 'BackLeft',
+                3: 'FrontLeft'},
+    'init_motorStatus': {1: {'controlMode': 0, 'targetVelocity': -13, 'force': 5},
+                            2: {'controlMode': 0, 'targetVelocity': 13, 'force': 5},
+                            0: {'controlMode': 0, 'targetVelocity': 2.7, 'force': 5},
+                            3: {'controlMode': 0, 'targetVelocity': 14.7, 'force': 5}},
+    'init_physical_para': {1: {'lateralFriction': .7, 'angularDamping': 0, 'linearDamping': 0, 'rollingFriction': 0, 'spinningFriction': 0},
+                            2: {'lateralFriction': .7, 'angularDamping': 0, 'linearDamping': 0, 'rollingFriction': 0, 'spinningFriction': 0},
+                            0: {'lateralFriction': .7, 'angularDamping': 0, 'linearDamping': 0, 'rollingFriction': 0, 'spinningFriction': 0},
+                            3: {'lateralFriction': .7, 'angularDamping': 0, 'linearDamping': 0, 'rollingFriction': 0, 'spinningFriction': 0}}}
